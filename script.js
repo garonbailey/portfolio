25a42a659e9aa568faf61817218ed4053915a9d2
garonbailey <bailegm@gmail.com>
2017-01-15 02:25:02 -0600

    basic index page re-design done; next is functionality to navigate

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-var $showButton = $('.show');
-var $about = $('#intro');
-var $portfolio = $('#portfolio');
+var $projectBubble = $('#projects');
+var $projectFilter = $('#project-filter');
 
-$showButton.on('click', function () {
-	$about.toggleClass('hidden');
-	$portfolio.toggleClass('hidden');
+var $bioBubble = $('#bio');
+var $bioFilter = $('#bio-filter');
+
+var $connectBubble = $('#connect');
+var $connectFilter = $('#connect-filter');
+
+var $contentTitle = $('#content-title');
+
+
+$projectBubble.on('mouseenter mouseleave', function () {
+	$projectFilter.toggleClass('hidden');
+});
+
+$projectBubble.on('mouseenter', function () {
+	$contentTitle.text('projects');
+});
+
+$projectBubble.on('mouseleave', function () {
+	$contentTitle.text('code. build. create.');
+});
+
+$bioBubble.on('mouseenter mouseleave', function () {
+	$bioFilter.toggleClass('hidden');
+});
+
+$bioBubble.on('mouseenter', function () {
+	$contentTitle.text('bio');
+});
+
+$bioBubble.on('mouseleave', function () {
+	$contentTitle.text('code. build. create.');
+});
+
+$connectBubble.on('mouseenter mouseleave', function () {
+	$connectFilter.toggleClass('hidden');
+});
+
+$connectBubble.on('mouseenter', function () {
+	$contentTitle.text('connect');
+});
+
+$connectBubble.on('mouseleave', function () {
+	$contentTitle.text('code. build. create.');
 });
 
 var $burger = $('.burger');
